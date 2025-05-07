6a1b62e19619131bcf63e310d70c94310e0531c2
ChristianGeng <christian.c.geng@gmail.com>
2025-05-07 14:43:45 +0200

    ivy is incompatible with vertico.

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -23,7 +23,8 @@
        ;; company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fonts +childframe +fuzzy)             ; a search engine for love and life
+       ;; ivy is incomplete with vertico
+       ;; (ivy +fonts +childframe +fuzzy)             ; a search engine for love and life
        ;;
        (corfu +dabbrev +icons)
        ;; ivy
