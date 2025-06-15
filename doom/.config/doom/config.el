12b8f5da6b42b5b5c28a9ee515030b9659d4202a
ChristianGeng <christian.c.geng@gmail.com>
2025-06-15 09:58:10 +0200

    Use consult for registers.

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -332,7 +332,7 @@ When mouse mode is disabled, also disable line numbers for easier copy-paste."
        :desc "Jump to register" "j" #'jump-to-register
        :desc "List registers" "l" #'list-registers
        :desc "Number to register" "n" #'number-to-register
-       :desc "Interactively choose a register" "r" #'counsel-register
+       :desc "Interactively choose a register" "r" #'consult-register
        :desc "View a register" "v" #'view-register
        :desc "Window configuration to register" "w" #'window-configuration-to-register
        :desc "Increment register" "+" #'increment-register
